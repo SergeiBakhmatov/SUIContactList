c372d4b078936cf4ba92693ee8bf8a909183c823
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-07-10 11:11:58 +0500

    Initial Commit

---
+++ SUIContactList/SUIContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIContactListApp.swift
+//  SUIContactList
+//
+//  Created by Sergei Bakhmatov on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUIContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
